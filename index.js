46559a18c962aced0c88d278e0012b2393f3daea
Denis Iogansen <pofigizm@gmail.com>
2017-11-06 03:56:18 +0300

    add config

--- index.js
+++ index.js
@@ -3,11 +3,56 @@ module.exports = {
   plugins: [
     'react'
   ],
-  settings: {
-    'import/resolver': {
-      'babel-module': {}
-    }
+  'env': {
+    'browser': true,
+    'jest': true
   },
-  rules: {
+  'parser': 'babel-eslint',
+  'globals': {
+    '__LOC__': true,
+    '__DEV__': true,
+    '__PROJECT__': true,
+    '__ROOT__': true
+  },
+  'rules': {
+    'comma-dangle': [
+      2,
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'ignore'
+      }
+    ],
+    'global-require': 0,
+
+    'import/extensions': 0,
+    'import/first': 0,
+    'import/no-duplicates': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+
+    'max-len': 1,
+    'no-case-declarations': 1,
+    'no-param-reassign': 1,
+    'no-prototype-builtins': 1,
+    'no-return-assign': 1,
+    'no-shadow': 1,
+    'no-underscore-dangle': 1,
+
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react/sort-comp': 0,
+
+    'semi': [2, 'never']
   }
 }
